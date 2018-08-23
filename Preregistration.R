bae0e2ce0a6721050c35692cc15250c6e7f53a67
Ihle <malika.ihle@ufl.edu>
2018-08-23 11:16:09 -0400

    tried to be crazy logical to build second table with dropping of prey depedening on first attack being on quinine termite or not, lots of ifs - commit this attempt before deleting it

--- Preregistration.R
+++ Preregistration.R
@@ -3,8 +3,8 @@
 #	 Preregistration Quinine induced Unpalatability 
 #  simulation of data to see whether planned analyses code works
 #	 Start : 20 August 2018
-#	 last modif : 
-#	 commit: start
+#	 last modif : 23 august 2018
+#	 commit: add dropYN for any attack in the trial
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
@@ -12,14 +12,14 @@
 rm(list = ls(all = TRUE))
 
 {# packages
-  library(lme4)
+  library(lme4) # for mixed effect models (not needed for simple glm)
   library(pbapply)
 }
 
 
 nF <- 60 # number of females to be tested
-pbrep <- 10000 # number of simulation replicates
-probs <- 0.7 # probability of DpdtYN being yes
+pbrep <- 2 # number of simulation replicates
+probs <- 0.7 # probability of FirstAttackYN being yes
 
 
 #Function to check number of significant result by chance
@@ -33,28 +33,87 @@ FocalTermiteColor <- rep(c('Green','Beige'), nF/2 ) # random (alternated) focal 
 FocalTermiteQuinine <- c(rep(c('1','0'), nF/4 ),rep(c('0','1'), nF/4 )) # whether or not the focal termite is the one with quinine
 DelayTo <- rnorm(nF, mean = 15, sd = 7) 
 
-MY_TABLE <- data.frame(FID, FTrt, DelayTo, FocalTermiteColor,FocalTermiteQuinine, row.names = NULL)
+MY_TABLE_FirstAttack <- data.frame(FID, FTrt, DelayTo, FocalTermiteColor,FocalTermiteQuinine, row.names = NULL)
 
 # simulation of an effect of quinine (say smell) onto that attack likelihood of the focal termite, if the termite has quinine, prob of attack of the focal is = probs
-for (i in 1:nrow(MY_TABLE)) {
-if (MY_TABLE$FocalTermiteQuinine[i] == 1)
-  MY_TABLE$DpdtYN[i] <- sample(c(1,0),1, prob = c(probs, 1-probs))
+for (i in 1:nrow(MY_TABLE_FirstAttack)) {
+if (MY_TABLE_FirstAttack$FocalTermiteQuinine[i] == 1)
+  MY_TABLE_FirstAttack$FirstAttackYN[i] <- sample(c(1,0),1, prob = c(probs, 1-probs))
   
-if (MY_TABLE$FocalTermiteQuinine[i] == 0)
-  MY_TABLE$DpdtYN[i] <- sample(c(0,1),1, prob = c(probs, 1-probs))
+if (MY_TABLE_FirstAttack$FocalTermiteQuinine[i] == 0)
+  MY_TABLE_FirstAttack$FirstAttackYN[i] <- sample(c(0,1),1, prob = c(probs, 1-probs))
 }
 
-#sunflowerplot(MY_TABLE$DpdtYN,FocalTermiteQuinine)
+#sunflowerplot(MY_TABLE_FirstAttack$FirstAttackYN,FocalTermiteQuinine)
 
 
-mod <- glm( DpdtYN ~ FocalTermiteColor + FocalTermiteQuinine, data = MY_TABLE)
-summary(mod)
+modfirstattack <- glm( FirstAttackYN ~ FocalTermiteColor + FocalTermiteQuinine, data = MY_TABLE_FirstAttack)
+summary(modfirstattack)
 
-## to get one sided test p value
-modp <-  coef(summary(mod))[-1, 4]
+## to p value
+modfirstattackp <-  coef(summary(modfirstattack))[-1, 4]
 
 
-return(list(modp))  
+
+for (i in 1:nrow(MY_TABLE_FirstAttack)){
+  
+if ((MY_TABLE_FirstAttack$FocalTermiteQuinine[i] == 1 & MY_TABLE_FirstAttack$FirstAttackYN[i] == 1) |  
+    (MY_TABLE_FirstAttack$FocalTermiteQuinine[i] == 0 & MY_TABLE_FirstAttack$FirstAttackYN[i] == 0))
+{ 
+  MY_TABLE_FirstAttack$TotalNumberofAttackInTrial[i] <- sample(c(1,2,3), 1, prob = c(0.2,0.7,0.1), replace=TRUE) # if first attack was on quinine termite, more likely to have a second attack (and sometimes even a third) on the palatable termite
+}
+  
+  if ((MY_TABLE_FirstAttack$FocalTermiteQuinine[i] == 1 & MY_TABLE_FirstAttack$FirstAttackYN[i] == 0) |  
+      (MY_TABLE_FirstAttack$FocalTermiteQuinine[i] == 0 & MY_TABLE_FirstAttack$FirstAttackYN[i] == 1))
+  { 
+    MY_TABLE_FirstAttack$TotalNumberofAttackInTrial[i] <- sample(c(1,2), 1, prob = c(0.9,0.1), replace=TRUE) # if first attack was on control termite, more likely that they consum it and we stop the trial
+  }  
+}
+  
+
+listperID <- split(MY_TABLE_FirstAttack,MY_TABLE_FirstAttack$FID)
+
+x <- MY_TABLE_FirstAttack[5,]
+
+listperID_fun = function(x)  {
+
+
+if (x$TotalNumberofAttackInTrial == 1)
+  {
+    if (x$FirstAttackYN == 1)
+        {out <- (c(x$FID,as.character(x$FTrt),x$FocalTermiteColor, x$FocalTermiteQuinine))}
+ 
+    if (x$FirstAttackYN == 0)
+      
+        {
+      
+         if (x$FocalTermiteColor == "Beige" & x$FocalTermiteQuinine == 1)
+               {out <-(c(x$FID,as.character(x$FTrt),'Green', '0'))}
+  
+         if(x$FocalTermiteColor == "Beige" & x$FocalTermiteQuinine == 0)
+               {out <-(c(x$FID,as.character(x$FTrt),'Green', '1'))}
+          
+         if(x$FocalTermiteColor == "Green" & x$FocalTermiteQuinine == 1)
+              {out <-(c(x$FID,as.character(x$FTrt),'Beige', 0))}
+ 
+        if(x$FocalTermiteColor == "Green" & x$FocalTermiteQuinine == 0)
+              {out <-(c(x$FID,as.character(x$FTrt),'Beige', 1))}   
+  
+       }
+  }
+  
+if (x$TotalNumberofAttackInTrial == 2) 
+  
+  
+  
+
+
+
+
+
+
+
+return(list(modfirstattackp))  
 }  
 
 OutputSimulation <- do.call(rbind, pbreplicate(pbrep,Simulate_and_analyse())) # collect all p values for both factors in the models

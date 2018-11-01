e1848bbfbb30a5f17c0c26ceb666c4821cf2aaf0
Ihle <malika.ihle@ufl.edu>
2018-11-01 14:36:29 -0400

    drop significantly more the non-bitrex ones ???

--- data_analysis.R
+++ data_analysis.R
@@ -7,15 +7,48 @@
 #	 commit: first commit
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-MyData <- read.csv(file="AllAttacks.csv", header=TRUE, sep=",")
+
+rm(list = ls(all = TRUE))
+
+# packages
+library(lme4)
+library(arm)
+
+AllAttacks <- read.csv(file="AllAttacks.csv", header=TRUE, sep=",")
+FocalTermiteAttack <- read.csv(file = "FocalTermiteAttack.csv", header=TRUE, sep=",")
+FirstAttacks <- read.csv(file = "FirstAttacks.csv", header=TRUE, sep=",")
 
 
-head(MyData)    
+
+head(AllAttacks)    
+head(FocalTermiteAttack)
+head(FirstAttacks)
 
 
 
+# model 1: reluctant to attack bitrex termite first ? especially if trained?
+head(FocalTermiteAttack)
+
+mod1 <- glm (FocalTermiteAttackedYN ~ FocalTermiteColor + FocalTermitePalatability*PriorExposureYN, family = 'binomial', data = FocalTermiteAttack)
+summary(mod1)
 
 
+
+# model 2: always drop bitrex termite? less so if trained/habituated?
+head(AllAttacks)
+
+mod2 <- glmer (DropYN ~ AttackedTermiteColor + AttackedTermitePalatability
+              # *PriorExposureYN 
+               + (1|FID), family = 'binomial', data = AllAttacks)
+summary(mod2)
+invlogit(coef(summary(mod2))[3,1])
+
+
+# explo: delay to first attack longer if attack the bitrex termite?
+head(FirstAttacks)
+
+mod3 <- lm(DelayToAttack ~ AttackedTermitePalatability, data = FirstAttacks)
+summary(mod3)
 
 
 

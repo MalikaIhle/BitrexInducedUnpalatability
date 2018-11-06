81dfc5fce4643d3757009c676efc2d831c5672f8
Ihle <malika.ihle@ufl.edu>
2018-11-06 17:24:47 -0500

    attempts to determine sample size for follow up experiments with higher bitrex concentration

--- data_analysis.R
+++ data_analysis.R
@@ -3,8 +3,8 @@
 #	 Preregistration manipulation color and unpalatability 
 #  data analysis
 #	 Start : 31 october 2018
-#	 last modif : 
-#	 commit: first commit
+#	 last modif : 20118 11 06
+#	 commit: determine sample size for follow up experiment with higher bitrex concentration
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
@@ -58,11 +58,51 @@ head(AllAttacks)
 
 mod2 <- glmer (DropYN ~ AttackedTermiteColor + AttackedTermitePalatability
                #*PriorExposureYN 
-               + (1|FID), family = 'binomial', data = AllAttacks)
+               + (1|FID)
+               ,family = 'binomial', data = AllAttacks)
 summary(mod2)
 
 sunflowerplot(AllAttacks$DropYN,AllAttacks$AttackedTermitePalatability)
 table(AllAttacks$Outcome,AllAttacks$AttackedTermitePalatability)
+
+DropUnpalatable <- invlogit(coef(summary(mod2))[1, 1]) # 39.4%
+DropUnpalatable_CI_low <-  invlogit(coef(summary(mod2))[1, 1]-coef(summary(mod2))[1, 2]*1.96) # 24.6%
+DropUnpalatable_CI_high <-invlogit(coef(summary(mod2))[1, 1]+coef(summary(mod2))[1, 2]*1.96) # 56.5%
+
+DropPalatable <- invlogit(coef(summary(mod2))[1, 1] + coef(summary(mod2))[3, 1]) # 9.9%
+DropPalatable_CI_low <-invlogit(coef(summary(mod2))[1, 1]+ coef(summary(mod2))[3, 1]-coef(summary(mod2))[3, 2]*1.96) # 4.6%
+DropPalatable_CI_high <-invlogit(coef(summary(mod2))[1, 1]+ coef(summary(mod2))[3, 1]+coef(summary(mod2))[3, 2]*1.96) # 20.1%
+
+# trying to follow guidelines:
+# https://besjournals.onlinelibrary.wiley.com/action/downloadSupplement?doi=10.1111%2F2041-210X.12306&file=mee312306-sup-0001-AppendixS1.pdf
+# from this paper
+# https://besjournals.onlinelibrary.wiley.com/doi/epdf/10.1111/2041-210X.12306
+
+OddsDrop <- ((invlogit(coef(summary(mod2))[1, 1])/(1-invlogit(coef(summary(mod2))[1, 1]))))/
+            ((invlogit(coef(summary(mod2))[1, 1] + coef(summary(mod2))[3, 1]))/(1-(invlogit(coef(summary(mod2))[1, 1] + coef(summary(mod2))[3, 1])))) # 5.9
+
+qlogis(invlogit(coef(summary(mod2))[1, 1] + coef(summary(mod2))[3, 1]))
+log(OddsDrop)
+
+# .... 
+
+# will consider mod2 as simple chisquare
+mod2glm <- glm (DropYN ~ AttackedTermitePalatability ,family = 'binomial', data = AllAttacks)
+summary(mod2glm)
+
+mod2quis <- chisq.test(AllAttacks$DropYN,AllAttacks$AttackedTermitePalatability)
+table(AllAttacks$Outcome,AllAttacks$AttackedTermitePalatability)
+# H0 reference: 10% dropping rate for control prey
+# H1, with 1% concentration, 40% dropping rate
+  
+w <- sqrt((0.10-0.40)^2/0.10  + (0.90-0.60)^2/0.90) # following formula: https://www.statmethods.net/stats/power.html
+
+library(pwr)
+?pwr.chisq.test
+pwr.chisq.test(w=0.5,N=, df=1, sig.level=0.05,power=0.8)
+pwr.chisq.test(w=0.9,N=, df=1, sig.level=0.05,power=0.8)
+pwr.chisq.test(w=1,N=, df=1, sig.level=0.05,power=0.8)
+
 
 
 

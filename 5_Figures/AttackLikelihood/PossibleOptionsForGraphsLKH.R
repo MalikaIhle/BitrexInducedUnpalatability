f8633e99bbad07e3e09b8fd94fae4bda6fa7f0d4
Ihle <malika.ihle@ufl.edu>
2019-03-21 20:05:45 -0400

    example for plots

---
+++ 5_Figures/AttackLikelihood/PossibleOptionsForGraphsLKH.R
@@ -0,0 +1,112 @@
+# create panelled figure for attack probability observed
+
+rm(list = ls(all = TRUE))
+
+
+library(here) # to get path of the Rproj root
+library(ggplot2) # to plot
+require(gridExtra) # for function gridarrange
+library(dplyr)
+
+FirstAttack1 <- read.csv(file = paste(here(),"3_ExtractedData/FirstAttacks/FirstAttacks1.csv", sep='/'), header=TRUE, sep=",")
+# FirstAttack1_5 <- read.csv(file = paste(here(),"3_ExtractedData/FirstAttacks/FirstAttacks1_5.csv", sep='/'), header=TRUE, sep=",")
+# FirstAttack2 <- read.csv(file = paste(here(),"3_ExtractedData/FirstAttacks/FirstAttacks2.csv", sep='/'), header=TRUE, sep=",")
+# FirstAttack3 <- read.csv(file = paste(here(),"3_ExtractedData/FirstAttacks/FirstAttacks3.csv", sep='/'), header=TRUE, sep=",")
+# FirstAttack3F <- read.csv(file = paste(here(),"3_ExtractedData/FirstAttacks/FirstAttacks3_Final.csv", sep='/'), header=TRUE, sep=",")
+
+
+
+contingency_tbl <-   data.frame(FirstAttack1 %>%
+    group_by(AttackedPalatability,PriorExposureYN) %>%
+   summarize( Count_perc = n()/nrow(FirstAttack1[FirstAttack1$PriorExposureYN==1,]), 
+              Count = n()))
+
+
+
+
+plot1 <- ggplot(contingency_tbl, aes(x = factor(AttackedPalatability), y = Count_perc, fill = factor(PriorExposureYN) )) +  
+  geom_bar(stat = "identity", position=position_dodge()) + 
+  labs(x = 'Palatability treatment', y= 'Prey probability of being attacked first')+
+  geom_hline(yintercept=0.5, linetype="dashed", color = "grey48") +
+  scale_y_continuous(limits = c(0, 0.6), breaks = c(0,0.1,0.2,0.3,0.4,0.5, 0.6), labels=scales::percent)+
+  scale_fill_grey(name = "Prior exposure to DB", labels = c('Naive', 'Trained')) +
+  scale_x_discrete(limits=c("1", "0"), labels=c("0" = "DB", "1" = "Control"))+
+  theme_classic()+
+  theme(panel.border = element_rect(colour = "black", fill=NA),
+        legend.position = c(0.25,0.9))
+  
+plot2 <- ggplot(contingency_tbl, aes(x = factor(AttackedPalatability), y = Count, fill = factor(PriorExposureYN) )) +  
+  geom_bar(stat = "identity", position=position_dodge()) + 
+  labs(x = 'Palatability treatment', y= 'Number of prey attacked first')+
+  scale_y_continuous(limits = c(0, 35), breaks = c(0,10,20,30))+
+  scale_fill_grey(name = "Prior exposure to DB", labels = c('Naive', 'Trained')) +  
+  scale_x_discrete(limits=c("1", "0"), labels=c("0" = "DB", "1" = "Control"))+
+  theme_classic()+
+  theme(panel.border = element_rect(colour = "black", fill=NA),
+        legend.position = c(0.25,0.85))
+       
+
+
+
+
+FocalAttacks1 <- read.csv(file = paste(here(),"3_ExtractedData/FocalAttacks/FocalAttacks1.csv", sep='/'), header=TRUE, sep=",")
+
+PrepDF_withtraining <- function(df){
+  df$FocalTermitePalatability[df$FocalTermitePalatability == "1"] <- 'Control'
+  df$FocalTermitePalatability[df$FocalTermitePalatability == "0"] <- 'DB'
+  df$PriorExposure[df$PriorExposureYN == 1] <- 'Trained'
+  df$PriorExposure[df$PriorExposureYN == 0] <- 'Naive'
+  df$Color[df$AttackedColor == "Brown"] <- 1
+  df$Color[df$AttackedColor == "Green"] <- 0
+  
+  
+  
+  df$PalatExp <- paste(df$FocalTermitePalatability, df$PriorExposure, sep="")
+  mod1 <- glm (FocalTermiteAttackedYN ~ -1+PalatExp + AttackedColor
+                # scale(Color)
+                 , family = 'binomial'
+               , data = df)
+  summary(mod1)
+  
+  effects_table <- as.data.frame(cbind(est=invlogit(summary(mod1)$coeff[,1]),
+                                       CIhigh=invlogit(summary(mod1)$coeff[,1]+summary(mod1)$coeff[,2]*1.96),
+                                       CIlow=invlogit(summary(mod1)$coeff[,1]-summary(mod1)$coeff[,2]*1.96)))
+  effects_table <- effects_table[-nrow(effects_table),]
+  effects_table$PriorExposure <- c("Naive", "Trained", "Naive","Trained")
+ effects_table$Palatability <- c("Control", "Control", "DB","DB")
+  
+  return(effects_table)
+}
+
+
+effects_table1 <- PrepDF_withtraining(FocalAttacks1)
+
+
+ plot3 <-   ggplot(data=effects_table1, aes(x=Palatability, y=est,colour=PriorExposure, shape = PriorExposure)) + 
+    scale_y_continuous(name="Prey probability of being attacked first", 
+                       limits=c(0, 1), breaks =c(0,0.25,0.50,0.75,1), labels=scales::percent)+ # 0.75 converted to 75%
+    theme_classic() + # white backgroun, x and y axis (no box)
+    labs(title = "DB solution concentration: 1%") +
+    
+    geom_errorbar(aes(ymin=CIlow, ymax=CIhigh, col=PriorExposure), width =0.4,na.rm=TRUE, position = position_dodge(width=0.5))+ # don't plot bor bars on x axis tick, but separate them (dodge)
+    geom_hline(yintercept=0.5, linetype="dashed", color = "grey48") +
+    geom_point(size =4, aes(shape=PriorExposure, col=PriorExposure), stroke = 1, position = position_dodge(width=0.5)) +
+    scale_colour_manual(name= "Prior exposure to DB", values=c("Black","Grey")) +
+    scale_shape_manual(name= "Prior exposure to DB", values=c(16,17))+ # duplicate title to combine legend
+    theme(panel.border = element_rect(colour = "black", fill=NA), # ad square box around graph 
+          legend.position=c(0.5,0.85),
+          legend.title = element_text(size=rel(0.8)),
+          legend.text = element_text(size=rel(0.7)),
+          legend.key.size = unit(0.8, 'lines'),
+          axis.title.x=element_text(size=10),
+          axis.title.y=element_text(size=10),
+          plot.title = element_text(hjust = 0.5, size = 10)) +
+    guides(shape = guide_legend(override.aes = list(linetype = 0, size = 2))) # remove bar o top of symbol in legend
+  
+    
+ plot1 <- ggplotGrob(plot1)
+ plot2 <- ggplotGrob(plot2)
+ plot3 <- ggplotGrob(plot3)
+    
+    grid.arrange(cbind(plot2,plot1, plot3, size="last"))
+    
--- Extraction_Sample_Analyses_Plot_1.R
+++ Extraction_Sample_Analyses_Plot_1.R
@@ -1,63 +1,23 @@
-#focal analyses bitrex 1%
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#	 Malika IHLE      malika_ihle@hotmail.fr
+#	 Data sampling for focal first attack, analyse and plot
+#  manipulation palatability with bitrex 1% and 3F% (with prior exposure for half the spiders)
+#	 Start : 21 march 2019
+#	 last modif : write function to apply to all DBs
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 
 rm(list = ls(all = TRUE))
 
 library(RODBC)
 library(here)
 library(pbapply)
+library(ggplot2) # to plot
+require(gridExtra) # for function gridarrange
 
-conDB= odbcConnectAccess2007(paste(here(),"1_RawData/VideoAnalyses_1BitrexTermites.accdb", sep='/'))
-sqlTables(conDB)	# list all the tables in the DB  
+FocalAttacks1 <- read.csv(file = paste(here(),"3_ExtractedData/FocalAttacks/FocalAttacks1.csv", sep='/'), header=TRUE, sep=",")
+FocalAttacks3F <- read.csv(file = paste(here(),"3_ExtractedData/FocalAttacks/FocalAttacks3F.csv", sep='/'), header=TRUE, sep=",")
 
-AllFemales <- sqlQuery(conDB, "
-                       SELECT Basic_Trials.Ind_ID AS FID, Basic_Trials.GroupName, Basic_Trials.SubGroupName, Behav_Female.ExcludeYN, Behav_Female.ReasonExclusion
-                       FROM Basic_Trials INNER JOIN Behav_Female ON Basic_Trials.Ind_ID = Behav_Female.FID;
-                       ")
-
-AllFemales <- AllFemales[AllFemales$ExcludeYN == 0,] # see remarks
-
-
-close(conDB)
-      
-FirstAttacks <- read.csv(file = "3_ExtractedData/FirstAttacks/FirstAttacks.csv", header=TRUE, sep=",")
-
-
-
-
-## create table 2 lines per test 
-  FocalTermiteAttack <- rbind(AllFemales[,c('FID', 'GroupName', 'SubGroupName')],AllFemales[,c('FID', 'GroupName', 'SubGroupName')])
-  FocalTermiteAttack$FocalTermiteColor <- c(rep('Brown',nrow(AllFemales)),rep('Green',nrow(AllFemales)))
-  FocalTermiteAttack <- FocalTermiteAttack[order(FocalTermiteAttack$FID),]
-  nrow(FocalTermiteAttack) # 200 looks good
-  
-  
-  FocalTermiteAttack <- merge(FocalTermiteAttack, FirstAttacks[,c('FID','AttackedTermiteColor')], by='FID', all.x=TRUE)
-  
-  
-  FocalTermiteAttack$FocalTermiteAttackedYN[FocalTermiteAttack$FocalTermiteColor == FocalTermiteAttack$AttackedTermiteColor] <- 1
-  FocalTermiteAttack$FocalTermiteAttackedYN[FocalTermiteAttack$FocalTermiteColor != FocalTermiteAttack$AttackedTermiteColor] <- 0
-  
-  
-  for (i in 1:nrow(FocalTermiteAttack)) {
-    
-    if(FocalTermiteAttack$SubGroupName[i] == "GreenDB" & FocalTermiteAttack$FocalTermiteColor[i] == 'Green')
-    {FocalTermiteAttack$FocalTermitePalatability[i] <- 0}
-    
-    if(FocalTermiteAttack$SubGroupName[i] == "GreenDB" & FocalTermiteAttack$FocalTermiteColor[i] == 'Brown')
-    {FocalTermiteAttack$FocalTermitePalatability[i] <- 1}
-    
-    if(FocalTermiteAttack$SubGroupName[i] == "BrownDB" & FocalTermiteAttack$FocalTermiteColor[i] == 'Brown')
-    {FocalTermiteAttack$FocalTermitePalatability[i] <- 0}
-    
-    if(FocalTermiteAttack$SubGroupName[i] == "BrownDB" & FocalTermiteAttack$FocalTermiteColor[i] == 'Green')
-    {FocalTermiteAttack$FocalTermitePalatability[i] <- 1}
-    
-  }
-  
-  FocalTermiteAttack$PriorExposureYN[FocalTermiteAttack$GroupName == 'DB'] <- 1
-  FocalTermiteAttack$PriorExposureYN[FocalTermiteAttack$GroupName == 'Water'] <- 0
-  
-  head(FocalTermiteAttack) 
   
 # select one at random (1000 times), run the model, save est, CI, p 
   
@@ -73,11 +33,8 @@ FirstAttacks <- read.csv(file = "3_ExtractedData/FirstAttacks/FirstAttacks.csv",
   FocalTermiteAttack <- do.call(rbind,lapply(FocalTermiteAttack,FocalTermiteAttack_fun))
   rownames(FocalTermiteAttack) <- NULL
   
-  FocalTermiteAttack$FocalTermitePalatability[FocalTermiteAttack$FocalTermitePalatability == "1"] <- 'Control'
-  FocalTermiteAttack$FocalTermitePalatability[FocalTermiteAttack$FocalTermitePalatability == "0"] <- 'DB'
-  FocalTermiteAttack$PriorExposure[FocalTermiteAttack$PriorExposureYN == 1] <- 'Trained'
-  FocalTermiteAttack$PriorExposure[FocalTermiteAttack$PriorExposureYN == 0] <- 'Naive'
-  FocalTermiteAttack$PalatExp <- paste(FocalTermiteAttack$FocalTermitePalatability, FocalTermiteAttack$PriorExposure, sep="")
+
+  FocalTermiteAttack$PalatExp <- paste(FocalTermiteAttack$FocalPalatabilityTreatment, FocalTermiteAttack$PriorExposure, sep="")
   
   modInter <- glm (FocalTermiteAttackedYN ~  PalatExp + FocalTermiteColor, family = 'binomial', data = FocalTermiteAttack)
   summary(modInter)
@@ -92,14 +49,14 @@ FirstAttacks <- read.csv(file = "3_ExtractedData/FirstAttacks/FirstAttacks.csv",
   
   }
   
-  effects_table1_list <- pbreplicate(1000, replicate(10, sample_focal(FocalTermiteAttack)))
+  effects_table1_list <- pbreplicate(1000, sample_focal(FocalAttacks1))
   
   effects_table1 <- Reduce(`+`, effects_table1_list) / length(effects_table1_list)
   effects_table1$PriorExposure <- c("Naive", "Trained", "Naive","Trained")
   effects_table1$Palatability <- c("Control", "Control", "DB","DB")
   
 
-   plot3 <- ggplot(data=effects_table1, aes(x=Palatability, y=est,colour=PriorExposure, shape = PriorExposure)) + 
+   plot1 <- ggplot(data=effects_table1, aes(x=Palatability, y=est,colour=PriorExposure, shape = PriorExposure)) + 
     scale_y_continuous(name="Prey probability of being attacked first", 
                        limits=c(0, 1), breaks =c(0,0.25,0.50,0.75,1), labels=scales::percent)+ # 0.75 converted to 75%
     theme_classic() + # white backgroun, x and y axis (no box)
@@ -120,7 +77,39 @@ FirstAttacks <- read.csv(file = "3_ExtractedData/FirstAttacks/FirstAttacks.csv",
           plot.title = element_text(hjust = 0.5, size = 10)) +
     guides(shape = guide_legend(override.aes = list(linetype = 0, size = 2))) # remove bar o top of symbol in legend
   
+
+   effects_table3F_list <- pbreplicate(5000, sample_focal(FocalAttacks3F))
+   
+   effects_table3F <- Reduce(`+`, effects_table3F_list) / length(effects_table3F_list)
+   effects_table3F$PriorExposure <- c("Naive", "Trained", "Naive","Trained")
+   effects_table3F$Palatability <- c("Control", "Control", "DB","DB")
+   
+   
+  plot3F <- ggplot(data=effects_table3F, aes(x=Palatability, y=est,colour=PriorExposure, shape = PriorExposure)) + 
+     scale_y_continuous(name="Prey probability of being attacked first", 
+                        limits=c(0, 1), breaks =c(0,0.25,0.50,0.75,1), labels=scales::percent)+ # 0.75 converted to 75%
+     theme_classic() + # white backgroun, x and y axis (no box)
+     labs(title = "DB solution concentration: 3%") +
+     
+     geom_errorbar(aes(ymin=CIlow, ymax=CIhigh, col=PriorExposure), width =0.4,na.rm=TRUE, position = position_dodge(width=0.5))+ # don't plot bor bars on x axis tick, but separate them (dodge)
+     geom_hline(yintercept=0.5, linetype="dashed", color = "grey48") +
+     geom_point(size =4, aes(shape=PriorExposure, col=PriorExposure), stroke = 1, position = position_dodge(width=0.5)) +
+     scale_colour_manual(name= "Prior exposure to DB", values=c("Black","Grey")) +
+     scale_shape_manual(name= "Prior exposure to DB", values=c(16,17))+ # duplicate title to combine legend
+     theme(panel.border = element_rect(colour = "black", fill=NA), # ad square box around graph 
+            axis.title.x=element_text(size=10),
+           axis.title.y=element_text(size=10),
+           plot.title = element_text(hjust = 0.5, size = 10)) +
+     guides(shape = guide_legend(override.aes = list(linetype = 0, size = 2))) # remove bar o top of symbol in legend
+   
+   
   
+  plot1 <- ggplotGrob(plot1)
+  plot3F <- ggplotGrob(plot3F)
   
+  #setEPS() 
+  #pdf("Fig2B.pdf", height=5, width=5)
+  grid.arrange(cbind(plot1,plot3F, size="last"))
+  #dev.off()
   
 
